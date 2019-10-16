dedf98b335601fd096b7e481b64ca47e1a36a332
chychen <jaycase.cs01@gmail.com>
2019-10-16 02:58:36 +0000

    map by batch bug fixed

--- tf_rlib/datasets/tf_datasets.py
+++ tf_rlib/datasets/tf_datasets.py
@@ -19,20 +19,27 @@ def get_cifar10():
 
     @tf.function
     def augmentation(x, y, pad=4):
-        x = tf.pad(x, [[0, 0], [pad, pad], [pad, pad], [0, 0]])
-        x = tf.image.random_crop(x, [x.shape[0], 32, 32, 3])
+        x = tf.pad(x, [[pad, pad], [pad, pad], [0, 0]])
+        x = tf.image.random_crop(x, [32, 32, 3])
         x = tf.image.random_flip_left_right(x)
         return x, y
+    
+    
+#     @tf.function
+#     def augmentation(x, y, pad=4):
+#         bs = tf.shape(x)[0]
+#         cropped_x = tf.pad(x, [[0, 0], [pad, pad], [pad, pad], [0, 0]])
+#         cropped_x = tf.image.random_crop(x, [bs, 32, 32, 3])
+#         choice_crop = tf.random.uniform(shape=[bs, 1, 1, 1], minval=0.0, maxval=1.0)
+#         x = tf.where(tf.less(choice_crop, 0.5), cropped_x, x)
+#         choice_flip = tf.random.uniform(shape=[bs, 1, 1, 1], minval=0.0, maxval=1.0)
+#         x = tf.where(tf.less(choice_flip, 0.5), tf.image.random_flip_left_right(x), x)
+#         return x, y
 
     train_dataset = tf.data.Dataset.from_tensor_slices(
         (train_data_x, train_data_y))
     valid_dataset = tf.data.Dataset.from_tensor_slices(
         (valid_data_x, valid_data_y))
-    train_dataset = train_dataset.shuffle(50000).batch(FLAGS.bs, drop_remainder=True).map(augmentation,num_parallel_calls=tf.data.experimental.AUTOTUNE)
-    valid_dataset = valid_dataset.batch(
-        FLAGS.bs, drop_remainder=False)
-#     train_dataset = train_dataset.cache().shuffle(50000).batch(FLAGS.bs, drop_remainder=True).map(augmentation,num_parallel_calls=tf.data.experimental.AUTOTUNE).prefetch(buffer_size=tf.data.experimental.AUTOTUNE)
-#     valid_dataset = valid_dataset.batch(
-#         FLAGS.bs, drop_remainder=False).cache().prefetch(
-#             buffer_size=tf.data.experimental.AUTOTUNE)
+    train_dataset = train_dataset.cache().shuffle(50000).map(augmentation,num_parallel_calls=tf.data.experimental.AUTOTUNE).batch(FLAGS.bs, drop_remainder=False)
+    valid_dataset = valid_dataset.batch(FLAGS.bs, drop_remainder=False).cache()
     return [train_dataset, valid_dataset]
