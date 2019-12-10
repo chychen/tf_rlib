c718bab9eabbb4f3d03dceb18b1b627037771929
chychen <jaycase.cs01@gmail.com>
2019-12-10 10:25:46 +0000

    FLAGS.bs=None by defulat

--- tf_rlib/__init__.py
+++ tf_rlib/__init__.py
@@ -57,7 +57,7 @@ flags.DEFINE_integer(
 flags.DEFINE_float('lr', 1e-3, 'Initial Learning Rate')
 flags.DEFINE_integer('epochs', 300, 'number of epochs for warming up')
 flags.DEFINE_integer('warmup', 5, 'number of epochs for warming up')
-flags.DEFINE_integer('bs', 128, 'global Batch Size for all gpus')
+flags.DEFINE_integer('bs', None, 'global Batch Size for all gpus')
 flags.DEFINE_float('adam_beta_1', 0.9, 'adam beta_1')
 flags.DEFINE_float('adam_beta_2', 0.999, 'adam beta_2')
 flags.DEFINE_float('adam_epsilon', 1e-8,
