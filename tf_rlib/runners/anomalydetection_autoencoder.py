walk around for distributed dataset api, minor bugs fixed about loss calculation

tf_rlib/runners/anomalydetection_autoencoder.py
@@ -71,7 +71,8 @@ class ADAERunner(runner.Runner):
         with tf.GradientTape() as tape:
             code, dec_x = self.model(x, training=True)
             loss = self.loss_object(y, dec_x)
-            loss = loss / FLAGS.bs  # distributed-aware
+            loss = tf.nn.compute_average_loss(
+                loss, global_batch_size=FLAGS.bs)  # distributed-aware
             regularization_loss = tf.nn.scale_regularization_loss(
                 tf.math.add_n(self.model.losses))  # distributed-aware
             total_loss = loss + regularization_loss
@@ -91,7 +92,8 @@ class ADAERunner(runner.Runner):
         """
         code, dec_x = self.model(x, training=False)
         loss = self.loss_object(y, dec_x)
-        loss = loss / FLAGS.bs
+        loss = tf.nn.compute_average_loss(
+            loss, global_batch_size=FLAGS.bs)  # distributed-aware
         return {'loss': [loss], 'mse': [y, dec_x], 'mae': [y, dec_x]}
 
     @property

tf_rlib/runners/regression.py
@@ -77,7 +77,8 @@ class RegressionRunner(runner.Runner):
         with tf.GradientTape() as tape:
             logits = self.model(x, training=True)
             loss = self.loss_object(y, logits)
-            loss = loss / FLAGS.bs  # distributed-aware
+            loss = tf.nn.compute_average_loss(
+                loss, global_batch_size=FLAGS.bs)  # distributed-aware
             regularization_loss = tf.nn.scale_regularization_loss(
                 tf.math.add_n(self.model.losses))  # distributed-aware
             total_loss = loss + regularization_loss
@@ -102,7 +103,8 @@ class RegressionRunner(runner.Runner):
         """
         logits = self.model(x, training=False)
         loss = self.loss_object(y, logits)
-        loss = loss / FLAGS.bs
+        loss = tf.nn.compute_average_loss(
+            loss, global_batch_size=FLAGS.bs)  # distributed-aware
         return {
             'loss': [loss],
             'mse': [y, logits],

tf_rlib/runners/runner.py
@@ -33,8 +33,6 @@ class Runner:
         FLAGS.exp_name = self.__class__.__name__
         self._validate_required_flags()
 
-        self.train_dataset = train_dataset
-        self.valid_dataset = valid_dataset
         utils.init_tf_rlib(show=True)
         self.strategy = tf.distribute.MirroredStrategy()
         LOGGER.info('Number of devices: {}'.format(
@@ -49,18 +47,15 @@ class Runner:
 
         with self._get_strategy_ctx():
             self.models, train_metrics, valid_metrics = self.init()
-            if self.strategy.num_replicas_in_sync > 1:
-                self.train_dataset_dtb = self.strategy.experimental_distribute_dataset(
+            if self.strategy.num_replicas_in_sync >= 1: #TODO: BUG!!!!
+                self.train_dataset = self.strategy.experimental_distribute_dataset(
                     train_dataset)
-                self.valid_dataset_dtb = self.strategy.experimental_distribute_dataset(
+                self.valid_dataset = self.strategy.experimental_distribute_dataset(
                     valid_dataset)
-            else:
-                self.train_dataset_dtb = train_dataset
-                self.valid_dataset_dtb = valid_dataset
 
             # weights init in first call()
             for key, model in self.models.items():
-                model_outs = model(next(iter(train_dataset))[0])
+                model_outs = model(next(iter(self.train_dataset))[0])
                 if type(model_outs) != tuple:
                     model_outs = tuple((model_outs, ))
                 outs_shapes = tuple((out.shape for out in model_outs))
@@ -189,7 +184,7 @@ class Runner:
                 self.metrics_manager.reset()
                 # train one epoch
                 for train_num_batch, (x_batch, y_batch) in enumerate(
-                        self.train_dataset_dtb):
+                        self.train_dataset):
                     self.step = self.step + 1
                     # train one step
                     if FLAGS.profile:
@@ -199,12 +194,14 @@ class Runner:
                     else:
                         self._train_step(x_batch, y_batch)
                     train_pbar.update(1)
+                    train_pbar.set_postfix(
+                        {'epoch/total': '{}/{}'.format(self.epoch, epochs)})
                 self._log_data(x_batch, y_batch, training=True)
 
                 # validate one epoch
-                if self.valid_dataset_dtb is not None:
+                if self.valid_dataset is not None:
                     for valid_num_batch, (x_batch, y_batch) in enumerate(
-                            self.valid_dataset_dtb):
+                            self.valid_dataset):
                         # validate one step
                         self._validate_step(x_batch, y_batch)
                         valid_pbar.update(1)