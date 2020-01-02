7317743304be2e8e2651803ee8c3729258833fdb
chychen <jaycase.cs01@gmail.com>
2020-01-02 06:04:24 +0000

    upsample api bug fixed: support 1D 3D

--- tf_rlib/layers/upsampling.py
+++ tf_rlib/layers/upsampling.py
@@ -9,8 +9,11 @@ class UpSampling(tf.keras.layers.Layer):
     def __init__(self, up_size=2):
         super(UpSampling, self).__init__()
         upsampling_op = layers.__dict__['UpSampling' + '{}D'.format(FLAGS.dim)]
-        self.upsampling_op = upsampling_op(size=up_size,
-                                           interpolation=FLAGS.interpolation)
+        if FLAGS.dim == 2:
+            self.upsampling_op = upsampling_op(
+                size=up_size, interpolation=FLAGS.interpolation)
+        else:  # APIs for 1D and 3D not support `interpolation` as a parameter
+            self.upsampling_op = upsampling_op(size=up_size)
 
     def call(self, x):
         x = self.upsampling_op(x)
