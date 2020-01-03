83adee2ed38ed4e9828087ecd73a00503029f1f4
chychen <jaycase.cs01@gmail.com>
2020-01-03 07:53:44 +0000

    use weight to solve solve the bias caused by aggregation before update_state

--- tf_rlib/metrics/rul_hitrate.py
+++ tf_rlib/metrics/rul_hitrate.py
@@ -19,14 +19,28 @@ class RULHitRate(MeanMetricWrapper):
 
     def hitrate(self, target_dim, margin, denorm_fn):
         def func(y_true, y_pred):
-            denorm_y_true = denorm_fn(y_true)
-            denorm_y_pred = denorm_fn(y_pred)
-            if target_dim is not None:
-                mae = tf.abs(denorm_y_true[:, target_dim] -
-                             denorm_y_pred[:, target_dim])
+            if y_true.shape[0] == 0:  # distributed awared
+                return tf.constant(0.0, dtype=y_true.dtype)
             else:
-                mae = tf.abs(denorm_y_true - denorm_y_pred)
-            hit_rate = tf.reduce_mean(tf.cast(mae < margin, dtype=tf.float32))
-            return hit_rate
+                denorm_y_true = denorm_fn(y_true)
+                denorm_y_pred = denorm_fn(y_pred)
+                if target_dim is not None:
+                    mae = tf.abs(denorm_y_true[:, target_dim] -
+                                 denorm_y_pred[:, target_dim])
+                else:
+                    mae = tf.abs(denorm_y_true - denorm_y_pred)
+
+                hit_rate = tf.reduce_mean(
+                    tf.cast(mae < margin, dtype=tf.float32))
+                return hit_rate
 
         return func
+
+    def update_state(self, y_true, y_pred, sample_weight=None):
+        """ Because the metric get aggregated before update_state, so we scalez the value by shape[0]
+        Ref: https://github.com/tensorflow/tensorflow/blob/r2.0/tensorflow/python/keras/metrics.py#L541
+        """
+        return super(RULHitRate,
+                     self).update_state(y_true,
+                                        y_pred,
+                                        sample_weight=y_true.shape[0])
