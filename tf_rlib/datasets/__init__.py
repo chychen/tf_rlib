cff57619d860a1d460612294d4807bbaef0ad68d
Warren <wtseng@nvidia.com>
2020-01-16 07:14:57 +0000

    add NVBump dataset

--- tf_rlib/datasets/__init__.py
+++ tf_rlib/datasets/__init__.py
@@ -3,3 +3,4 @@ from tf_rlib.datasets.tf_datasets import Omniglot, Cifar10
 from tf_rlib.datasets.cell import Cell_segmentation
 from tf_rlib.datasets.mvtec_datasets import MVTecDS
 from tf_rlib.datasets.phm2018 import PHM2018
+from tf_rlib.datasets.bump import NVBump
---
+++ tf_rlib/datasets/bump.py
@@ -0,0 +1,84 @@
+import tensorflow as tf
+from tensorflow.keras.utils import to_categorical
+import numpy as np
+from tf_rlib import datasets
+from absl import flags, logging
+
+FLAGS = flags.FLAGS
+LOGGER = logging.get_absl_logger()
+
+class NVBump(datasets.Dataset):
+    def __init__(self, path='/mount/data/SPIL/data/preprocessed/'):
+        self.path = path
+        super(NVBump, self).__init__()
+        self.dsets = self._get_dsets()
+        
+    def get_data(self):
+        return self.dsets
+    
+    def _get_dsets(self):
+        # load data
+        X_defect = np.load('/mount/data/SPIL/Data/preprocessed/X_defect.npy')[:,172:428,172:428]
+        X_pass = np.load('/mount/data/SPIL/Data/preprocessed/X_pass.npy')[:,172:428,172:428]
+        X_defect_valid = np.load('/mount/data/SPIL/Data/preprocessed/X_defect_val.npy')
+        X_pass_valid = np.load('/mount/data/SPIL/Data/preprocessed/X_pass_val.npy')
+        # Spilt more data to be validated
+        idx = np.arange(len(X_defect))
+        np.random.shuffle(idx)
+        X_defect_valid = np.concatenate([X_defect_valid, X_defect[:305]])
+        X_defect = X_defect[305:]
+        # get generator
+        gen_train = self.get_generator(X_defect, X_pass)
+        gen_valid = self.get_generator(X_defect_valid, X_pass_valid)
+        # tf dataset
+        train_dataset = tf.data.Dataset.from_generator(gen_train,
+                                                       (tf.float32, tf.float32),
+                                                       (tf.TensorShape([None, 224, 224, 3]), tf.TensorShape([None])))
+        valid_dataset = tf.data.Dataset.from_generator(gen_valid,
+                                                       (tf.float32, tf.float32),
+                                                       (tf.TensorShape([None, 224, 224, 3]), tf.TensorShape([None])))
+        train_dataset = train_dataset.cache().shuffle(
+            2000).prefetch(buffer_size=tf.data.experimental.AUTOTUNE)
+        valid_dataset = valid_dataset.cache().prefetch(
+                buffer_size=tf.data.experimental.AUTOTUNE)
+        return [train_dataset, valid_dataset]
+    
+    def random_crop(self, img, random_crop_size):
+        height, width = img.shape[0], img.shape[1]
+        dy, dx = random_crop_size
+        x = np.random.randint(0, width - dx + 1)
+        y = np.random.randint(0, height - dy + 1)
+        return img[y:(y+dy), x:(x+dx), :]
+
+    def augment(self, X):
+        # flip
+        toflip = np.random.uniform()
+        if toflip > .67:
+            X = np.flip(X, axis=0)
+        elif toflip > .33:
+            X = np.flip(X, axis=1)
+        X = self.random_crop(X, (224, 224))
+        X = tf.keras.preprocessing.image.random_brightness(X, (.8, 1.2))
+        return X
+
+    def sample_data(self, X_d, X_p):
+        np.random.shuffle(self.idx_d)
+        np.random.shuffle(self.idx_p)
+        x, y = [], []
+        for i in range(FLAGS.bs//2):
+            xp = X_p[self.idx_p[i]]
+            xd = X_d[self.idx_d[i]]
+            xp = (self.augment(xp)-127.5)/127.5
+            xd = (self.augment(xd)-127.5)/127.5
+            x += [xp, xd]
+            y += [0, 1]
+        return np.array(x), np.array(y)
+
+    def get_generator(self, X_d, X_p):
+        def gen():
+            self.idx_d = np.arange(len(X_d))
+            self.idx_p = np.arange(len(X_p))
+            for _ in range(len(X_d)//FLAGS.bs):
+                x, y = self.sample_data(X_d, X_p)
+                yield x, y.astype('float32')
+        return gen
