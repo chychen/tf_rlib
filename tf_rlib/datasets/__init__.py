3a143913241d1cc6399be3fda274c0f2b348d11b
chychen <jaycase.cs01@gmail.com>
2020-01-22 14:47:24 +0000

    add tfrecord example, use tfrecord on cifar10 speedup 5%-10%

--- tf_rlib/datasets/__init__.py
+++ tf_rlib/datasets/__init__.py
@@ -1,5 +1,5 @@
 from tf_rlib.datasets.dataset import Dataset
-from tf_rlib.datasets.tf_datasets import Omniglot, Cifar10
+from tf_rlib.datasets.tf_datasets import Omniglot, Cifar10, Cifar10Numpy
 from tf_rlib.datasets.cell import Cell_segmentation
 from tf_rlib.datasets.mvtec_datasets import MVTecDS
 from tf_rlib.datasets.phm2018 import PHM2018
