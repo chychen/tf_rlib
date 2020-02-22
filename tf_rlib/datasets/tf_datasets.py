8a80e4df7f9c23707902a166b5f42da34b2b0f8d
chychen <jaycase.cs01@gmail.com>
2020-02-22 05:28:31 +0000

    add mnist vae demo code

--- tf_rlib/datasets/tf_datasets.py
+++ tf_rlib/datasets/tf_datasets.py
@@ -12,6 +12,39 @@ FLAGS = flags.FLAGS
 LOGGER = logging.get_absl_logger()
 
 
+class Mnist(datasets.Dataset):
+    def __init__(self):
+        super(Mnist, self).__init__()
+
+    def get_data(self):
+        (train_images, _), (test_images,
+                            _) = tf.keras.datasets.mnist.load_data()
+        train_images = train_images.reshape(train_images.shape[0], 28, 28,
+                                            1).astype('float32')
+        test_images = test_images.reshape(test_images.shape[0], 28, 28,
+                                          1).astype('float32')
+
+        # Normalizing the images to the range of [0., 1.]
+        train_images /= 255.
+        test_images /= 255.
+
+        # Binarization
+        train_images[train_images >= .5] = 1.
+        train_images[train_images < .5] = 0.
+        test_images[test_images >= .5] = 1.
+        test_images[test_images < .5] = 0.
+
+        TRAIN_BUF = 60000
+        BATCH_SIZE = FLAGS.bs
+        TEST_BUF = 10000
+
+        train_dataset = tf.data.Dataset.from_tensor_slices(
+            (train_images, train_images)).shuffle(TRAIN_BUF).batch(BATCH_SIZE)
+        test_dataset = tf.data.Dataset.from_tensor_slices(
+            (test_images, test_images)).shuffle(TEST_BUF).batch(BATCH_SIZE)
+        return train_dataset, test_dataset
+
+
 class Omniglot(datasets.Dataset):
     SAVE_PATH = '/tmp/omniglot/'
 
--- tf_rlib/losses/__init__.py
+++ tf_rlib/losses/__init__.py
@@ -2,3 +2,4 @@ from tf_rlib.losses.custom_loss import CustomLoss
 from tf_rlib.losses.dice_loss import DiceLoss
 from tf_rlib.losses.mse_loss import MSELoss
 from tf_rlib.losses.mae_loss import MAELoss
+from tf_rlib.losses.vae_loss import VAELoss
